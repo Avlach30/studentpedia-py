4cdaa5c7dc2889cacf5345e3b940a9421d1ae9c1
Muhammad Zakia Avlach <19mzaki@gmail.com>
2023-02-04 11:49:21 +0700

    develop handler when user choose 3 for delete some data

--- main.py
+++ main.py
@@ -29,3 +29,14 @@ elif selectMenu == 2:
     else:
         print("Sorry, {0} is not found from datastore".format(student))
         exit()
+elif selectMenu == 3:
+    student = input("Input a student name which is you want to delete it: ")
+    isExisting = students.count(student.lower())
+    if isExisting == 1: # If inputted data is available on datastore
+        idx = students.index(student) # Get index number in datastore
+        students.pop(idx)
+        print("List a new students data after updated: {0}".format(students))
+        exit()
+    else:
+        print("Sorry, {0} is not found from datastore".format(student))
+        exit()
