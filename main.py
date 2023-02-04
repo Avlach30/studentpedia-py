b4324619f997733ab99bac6b4822ec683ecd9b5d
Muhammad Zakia Avlach <19mzaki@gmail.com>
2023-02-04 11:33:02 +0700

    develop handler when user choose 1 for add new data

--- main.py
+++ main.py
@@ -6,3 +6,14 @@ selectMenu = int(input("Select number for access a feature \n1. '1' for input ne
 
 if selectMenu != 1 | selectMenu != 2 | selectMenu != 3:
     exit() # Stop a runned process
+
+if selectMenu == 1:
+    newStudent = input("Input a new student name: ")
+    isExisting = students.count(newStudent.lower())
+    if isExisting == 1: # If inputted is already on datastore
+        print("Sorry, {0} is already on datastore".format(newStudent))
+        exit()
+    else:
+        students.append(newStudent.lower())
+        print("List a new students: {0}".format(students))
+        exit()
