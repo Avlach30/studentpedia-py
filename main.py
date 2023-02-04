607cc80312f62745969aedede1780d2a91511fb7
Muhammad Zakia Avlach <19mzaki@gmail.com>
2023-02-04 11:51:08 +0700

    develop handler when user not choose 1, 2, or 3 for access the menu

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ print("Hello! Welcome to simple student data management app 'StudentPedia'")
 print(students)
 selectMenu = int(input("Select number for access a feature \n1. '1' for input new student data\n2. '2' for update existing student data\n3. '3' for delete existing student data\n:"))
 
-if selectMenu != 1 | selectMenu != 2 | selectMenu != 3:
-    exit() # Stop a runned process
-
 if selectMenu == 1:
     newStudent = input("Input a new student name: ")
     isExisting = students.count(newStudent.lower())
@@ -40,3 +37,6 @@ elif selectMenu == 3:
     else:
         print("Sorry, {0} is not found from datastore".format(student))
         exit()
+else:
+    print("Sorry, you only choose one of 1, 2, or 3 for access the menu")
+    exit() # Stop a runned process
